3cd321ed33c51fff72d4eefe9a72f852ecf65e22
maschepers <m.professional188@gmail.com>
2021-11-18 20:53:38 +0100

    CorrectionsAndFeatureHandler
    Split up the main in several smaller functions and added return statements. Also created a feature handler which gives the final file. Created a separate file (location converter) which then grabs the coordenates and extracts the bases from the DNA_sequence.

---
+++ LocationConverter.py
@@ -0,0 +1,24 @@
+locations = '1:60,61:120,121:180,181:240'
+DNA = []
+with open('output_sequence.txt', 'r') as origin:
+    for line in origin:
+        line = line.rstrip()
+        DNA.append(line)
+    DNA = ''.join(DNA)
+    print(len(DNA))
+
+# print(DNA[265-1:402])
+
+# print(DNA)
+
+def location_converter(locations):
+    splitLocs = locations.split(",")
+    for i in splitLocs:
+        begin, end = i.split(':')
+        begin = int(begin)-1 # Python indexing starts with 0, whereas DNA counting with 1.
+        end = int(end) # Last item is non-inclusive
+        print(begin, end)
+        print(DNA[begin:end])
+        print(len(DNA[begin:end]))
+
+location_converter(locations)
--- main.py
+++ main.py
@@ -1,61 +1,79 @@
-# def origin_handler(file):
-#     # Creating null variables for loop
-#     index_origin_begin = 0
-#     index_origin_end = 0
-#     line_number = 0
-#     # Importing regex to remove all non-alphabet characters from origin
-#     import re # Add to class, make it global inside the class
-#
-#     # Opens and identifies the location of the origin
-#     try:
-#         with open(file, 'r') as gbfile:
-#             for line in gbfile:  # Opens lines in the text one by one
-#                 line_number += 1  # Keeping count of the line number for later indexing
-#                 line = line.rstrip()  # Removing any trailing characters
-#                 # Following loop selects all the lines containing the origin
-#                 if "ORIGIN" in line:
-#                     index_origin_begin = line_number + 1  # Finds line number just before origin and adds 1.
-#                 elif "//" in line:
-#                     index_origin_end = line_number - 1  # Finds line number right after origin and subtracts 1.
-#     except(FileNotFoundError, IOError):
-#         return "File not found or incorrect file path"
-#         # Should I maybe add this to the output file?
-#         # Printing kills the whole process, so should I avoid that as well?
-#
-#     # Uses the line numbers from above code to select origin and then removes all non-alphabet characters
-#     with open(file, 'r') as gbfile:
-#         output_sequence = open("output_sequence.txt", "w")  # Creates or overwrites existing file for sequence output
-#         line_number = 0
-#         for line in gbfile:
-#             line_number += 1
-#             line = line.rstrip()  # Removing any trailing characters
-#             if index_origin_begin <= line_number <= index_origin_end:
-#                 regex_string = re.compile('[^a-zA-Z]')
-#                 line = re.sub(regex_string, "", line)
-#                 with open("output_sequence.txt", "a") as output_file:
-#                     output_file.write(line + "\n")
-#         output_sequence.close()
-#
-# origin_handler('Testfile.gp')
-#
-# def definition_handler(file):
-#     import re  # Add to class, make it global inside the class
-#     output_definition = open("output_definition.txt", "w") # Creates or overwrites existing file for definition output
-#     with open(file, 'r') as gbfile:
-#         for line in gbfile:
-#             if 'DEFINITION' in line:
-#                 line = line.rstrip()  # Removing any trailing characters
-#                 regex_string = re.compile('DEFINITION\s{2}')
-#                 line = re.sub(regex_string, "", line)
-#                 with open("output_definition.txt", "a") as output_file:
-#                     output_definition.write(line + "\n")
-#             else:
-#                 pass
-#     output_definition.close()
-#
+def origin_handler(file):
+    # Creating null variables for loop
+    index_origin_begin = 0
+    index_origin_end = 0
+    line_number = 0
+    # Importing regex to remove all non-alphabet characters from origin
+    import re # Add to class, make it global inside the class
+
+    # Opens and identifies the location of the origin
+    try:
+        with open(file, 'r') as gbfile:
+            for line in gbfile:  # Opens lines in the text one by one
+                line_number += 1  # Keeping count of the line number for later indexing
+                line = line.rstrip()  # Removing any trailing characters
+                # Following loop selects all the lines containing the origin
+                if "ORIGIN" in line:
+                    index_origin_begin = line_number + 1  # Finds line number just before origin and adds 1.
+                elif "//" in line:
+                    index_origin_end = line_number - 1  # Finds line number right after origin and subtracts 1.
+    except(FileNotFoundError, IOError):
+        return "File not found or incorrect file path"
+        # Should I maybe add this to the output file?
+        # Printing kills the whole process, so should I avoid that as well?
+
+    # Uses the line numbers from above code to select origin and then removes all non-alphabet characters
+    dna_list = []
+    with open(file, 'r') as gbfile:
+        output_sequence = open("output_sequence.txt", "w")  # Creates or overwrites existing file for sequence output
+        line_number = 0
+        for line in gbfile:
+            line_number += 1
+            line = line.rstrip()  # Removing any trailing characters
+            if index_origin_begin <= line_number <= index_origin_end:
+                regex_string = re.compile('[^a-zA-Z]')
+                line = re.sub(regex_string, "", line)
+                # print(line)
+                dna_list.append(line)
+                with open("output_sequence.txt", "a") as output_file:
+                    output_file.write(line + "\n")
+        output_sequence.close()
+    # print(dna_list)
+origin_handler('Testfile.gp')
+
+def definition_handler(file):
+    import re  # Add to class, make it global inside the class
+    tmp_definition = []
+    line_found = False
+
+    with open(file, 'r') as gbfile:
+        for line in gbfile:
+            line = line.rstrip()
+            if 'DEFINITION' in line: # Finds the first line containing the definition
+                tmp_definition = [line]
+                line_found = True
+            elif line_found == True and not line.endswith('.'): # If definition has been found, appends line.
+                tmp_definition.append(line)
+            elif line_found == True and line.endswith('.'):
+                # If definition has been found and line ends with a '.' end of definition has been reached.
+                tmp_definition.append(line)
+                line_found = False # Set to false so program won't append any more lines.
+            else:
+                pass
+
+    tmp_definition = ''.join(tmp_definition)
+    # print(tmp_definition)
+    # Substitute DEFINITION and empty spaces by nothing
+    regex_str_one = re.compile('DEFINITION\s{2,}')
+    tmp_definition = re.sub(regex_str_one, "", tmp_definition)
+    regex_str_two = re.compile('\s{2,}')
+    tmp_definition = re.sub(regex_str_two, " ", tmp_definition)
+
+    return(tmp_definition)
+
 # definition_handler('Testfile.gp')
 
-def feature_handler(file):
+def feature_index_handler(file):
     import re
     feature_begin = 0
     feature_end = 0
@@ -63,45 +81,25 @@ def feature_handler(file):
     with open(file, 'r') as gbfile:
         line_number = 0
         for line in gbfile:
+            line_number += 1 # Keeps track of line number for every line
             if "FEATURES" in line:
-                line_number += 1
-                feature_begin = line_number
+                # print(line, line_number)
+                feature_begin = line_number + 1 # The features start the line after the header has been found.
             elif 'ORIGIN' in line:
-                feature_end = line_number - 1
+                # print(line, line_number)
+                feature_end = line_number - 1 # Line containing the origin, the features end one line before.
                 break
             else:
-                line_number += 1
-
-    # regex_string = re.compile('DEFINITION\s{2}')
-    # line = re.sub(regex_string, "", line)
-
-    line_number = 0
-    feature_list = []
-    # index_organism = []
-
-    with open(file, 'r') as gbfile:
-
-        for line in gbfile:
-            if line_number < feature_begin:
-                line_number += 1
                 pass
-            elif feature_begin <= line_number <= feature_end:
-                line_number += 1
-                line = line.strip()
-                line = re.sub(' +', "$", line)  # Replaces 1 or more empty spaces by $
-                feature_list.append(line)  # Appends line to feature_list
+    return(feature_begin, feature_end)
 
-    # for i in feature_list:
-    #     if "/organism=" in i:
-    #         index_organism = feature_list.index(i)
-    #     else:
-    #         pass
+# feature_index_handler('Testfile.gp')
 
+def location_handler(feature_list):
     location_tmp_parts = []  # Variable to hold parts of locations
     feature_list_edited = []  # Feature list to hold corrected locations
+    import re
 
-    #  Following statement checks for locations that span more then 1 line
-    #  and makes them into a single element in the list.
     for i in feature_list:
         if i.endswith(','):
             location_tmp_parts.append(i)
@@ -114,37 +112,72 @@ def feature_handler(file):
         else:
             feature_list_edited.append(i)
 
-    # print(feature_list_edited)
-
-    feature_list_final = []
-
     with open("final_file.txt", "w") as final_file:
         for i in feature_list_edited:
             if '..' in i and '/' not in i:
                 index_i = feature_list_edited.index(i) + 1
                 # print(index_i)
                 feature_name, location = i.split('$', 1)
-                ## Call function that transforms b into actual sequence
-                # b = b.blablabla
-                # final_file.write('>' + feature_name + ' ' + feature_list_edited[index_i] + "\n" + location + "\n"*2)
                 qualifier = re.sub("\$", ' ', feature_list_edited[index_i])
                 location = re.sub("\.\.", ':', location)
-                print(location)
-                # print('>' + feature_name + ' ' + qualifier + "\n" + location + "\n"*2)
-                break
-    with open("final_file.txt", "r") as final_file:
-        for line in final_file:
-            print(line)
+                if 'join' in location:
+                    location = re.sub('join\(|\)', "", location)  # Replaces 1 or more empty spaces by $
+                    # print(location)
+                elif 'complement' in location:
+                    location = re.sub('complement\(|\)', "", location)  # Replaces 1 or more empty spaces by $
+                    # print(location)
+                else:
+                    # print(location)
+                    pass
+                # print(qualifier)
+                # print(location)
+                ## Call function that transforms b into actual sequence
+                # b = b.blablabla
+                final_file.write('>' + feature_name + ' ' + feature_list_edited[index_i] + "\n" + location + "\n"*2)
+                # print('>' + feature_name + ' ' + feature_list_edited[index_i] + "\n" + location + "\n"*2)
 
-    sequence_string = []
-    with open('output_sequence.txt', 'r') as sequence_str:
-        for line in sequence_str:
-            line = line.strip()
-            print(line)
-            sequence_string += line
-            sequence_string = ''.join(sequence_string)
-    print(sequence_string)
+def feature_handler(file):
+    feature_list = []
+    index_organism = []
+    import re
+
+    begin_end_feature = feature_index_handler(file) # Returns the beginning and end of the features
+    feature_begin, feature_end = begin_end_feature[0], begin_end_feature[1]
+    # print(feature_begin, feature_end) # Checking whether features have properly been indexed.
+
+    with open(file, 'r') as gbfile:
+        line_number = 0
+        for line in gbfile:
+            line_number += 1
+            if line_number < feature_begin:
+                pass
+            elif feature_begin <= line_number <= feature_end:
+                line = line.strip()
+                line = re.sub(' +', "$", line)  # Replaces 1 or more empty spaces by $
+                feature_list.append(line)  # Appends line to feature_list
+    feature_list = location_handler(feature_list) #  Checks for locations that span more then 1 line and unites them.
+
+    # print(feature_list)
 
 feature_handler('Testfile.gp')
 
-# for i in feature_list_edited:
+#
+# # feature_handler('CFTR_DNA.gb')
+#
+#     sequence_string = []
+#     with open('output_sequence.txt', 'r') as sequence_str:
+#         for line in sequence_str:
+#             line = line.strip()
+#             # print(line)
+#             sequence_string += line
+#             sequence_string = ''.join(sequence_string)
+#             begin, end = location.split(':', 1)
+#             begin = int(begin) - 1 # Python indexing starts at zero, so -1
+#             end = int(end) # Python indexing does not include the last item, thus -1 not necessary
+#     print(begin, end)
+#     print(sequence_string)
+#     print(sequence_string[begin:end])
+#
+# feature_handler('Testfile.gp')
+#
+# # for i in feature_list_edited:
